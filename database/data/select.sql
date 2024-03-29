ba9359e1ccdd2d7377aed88d5e079b903ae88e69
mveraloo <77229096+mveraloo@users.noreply.github.com>
2024-03-29 15:10:04 -0600

    Delete database directory

--- database/data/select.sql
+++
@@ -1,8 +0,0 @@
-SELECT u.user_id, u.first_name, u.last_name, c.quantity, o.order_date, p.payment_amount
-FROM UserAccount u
-INNER JOIN Cart c
-ON u.user_id = c.user_id
-inner JOIN Orders o
-ON c.cart_id = o.cart_id
-inner JOIN Payment p
-On o.order_id = p.order_id;
